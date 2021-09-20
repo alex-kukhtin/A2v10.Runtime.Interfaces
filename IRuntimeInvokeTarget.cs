01b23320341e5b7ba72158892a39034f0b8fc998
Аlex Kukhtin <alex.kukhtin@outlook.com>
2021-09-20 07:45:05 +0300

    Add project files.

---
+++ IRuntimeInvokeTarget.cs
@@ -0,0 +1,13 @@
+﻿// Copyright © 2021 Alex Kukhtin. All rights reserved.
+
+using System;
+using System.Dynamic;
+using System.Threading.Tasks;
+
+namespace A2v10.Runtime.Interfaces
+{
+	public interface IRuntimeInvokeTarget
+	{
+		Task<ExpandoObject> InvokeAsync(String method, ExpandoObject parameters);
+	}
+}
